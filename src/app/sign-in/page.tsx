3f56b55f48b964d3265b9dbf7bb52011e4e7a808
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-12 19:53:46 -0400

    designing sign-in page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default async function Page() {
+    return (
+        <div className="w-full min-h-screen grid place-items-center">
+            {/* Sign In Box */}
+            <div className="max-w-xs w-full border border-zinc-200 shadow shadow-sm flex flex-col gap-4 p-6">
+                <p className="text-2xl font-medium">Sign In</p>
+
+                <button className="px-4 py-2 bg-violet-500 text-white font-medium rounded-lg">
+                    Sign in with Discord
+                </button>
+            </div>
+        </div>
+    )
+}
