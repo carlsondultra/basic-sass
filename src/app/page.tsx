a6e745e92ec2f05a3b0c634ee56aa64853a56625
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-14 18:28:36 -0400

    adding Stripe pricing table component on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import Image from 'next/image'
+import { StripePricingTable } from '@/helpers/components'
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-2xl m-auto px-4">
+      <div className="flex flex-col my-4">
+        <h1 className="text-5xl font-semibold">Basic SaaS</h1>
+        <p className="text-black/80 pb-2">Please subscribe to my basic SaaS.</p>
+        <StripePricingTable />
+      </div>
     </main>
   )
 }
